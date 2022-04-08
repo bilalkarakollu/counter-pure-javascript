fc2298cb64049d388171d1f793a86dc14134a441
bilalkarakollu <karakollubilal@gmail.com>
2022-04-08 11:06:28 +0300

    localstroge added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-let count = 0;
+const getCount = localStorage.getItem('count');
+let count = getCount ? getCount : 0;
 const incrementBtn = document.querySelector('#increment');
 const decrementBtn = document.querySelector('#decrement');
 const countDOM = document.querySelector('#count');
+
+countDOM.innerHTML = count; 
 
 incrementBtn.addEventListener('click', clickEvent)
 decrementBtn.addEventListener('click', clickEvent)
@@ -10,5 +13,6 @@ function clickEvent(){
 
     this.id === 'increment' ? count++ : count--;
     countDOM.innerHTML = count;
+    localStorage.setItem('count',count)
 
 }
